c557179487e5ad66452cb3f8d880adf4a96f519e
Randy Leon <randy.leon123@outlook.com>
2019-03-02 23:26:39 -0500

    save changes

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,12 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp3
+namespace AverageNonSpecified
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("Press enter to begin.\nType 101 when complete");
+            Console.ReadLine();
+            Console.WriteLine("Enter grade");
+            double value = double.Parse(Console.ReadLine());
+            double sum = 0;
+            double totalNumbers = 0;
+
+
+            while (value <= 100 && value >= 0 && totalNumbers < 10)
+            {
+
+                sum += value;
+                totalNumbers++;
+
+                Console.WriteLine("Enter grade");
+                value = double.Parse(Console.ReadLine());
+
+            }    
+
+            double average = sum / totalNumbers;
+            double Grade = average;
+
+            if (Grade > 89)
+            {
+                Console.WriteLine("Grade = A");
+            }
+
+            else if (Grade > 79)
+            {
+                Console.WriteLine("Grade = B");
+            }
+
+            else if (Grade > 69)
+            {
+                Console.WriteLine("Grade = C");
+            }
+
+            else if (Grade > 59)
+            {
+                Console.WriteLine("Grade = D");
+            }
+
+            else
+            {
+                Console.WriteLine("Grade = F");
+            }
+
+                 Console.WriteLine($"Average = {average}");
+                 Console.WriteLine($"Total assignments graded = {totalNumbers}");
+
         }
     }
 }
